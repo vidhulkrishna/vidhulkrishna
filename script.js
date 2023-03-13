d573b886d24e8cebddd173f6ed5400a71c589438
venkiteshps <psvenkiduzz@gmail.com>
2023-03-13 02:54:22 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,89 @@
+// // Datatypes
+// var sum=5+10;
+// console.log(sum);
+// var mul=5*10;
+// console.log(mul);
+// var sub=10-5;
+// console.log(sub);
+// document.write("sum ="+ sum)
+// document.write("mul ="+ mul)
+// document.write("sub ="+ sub)
+// document.write(`sum is ${sum}`)
+// var students={
+//     name:"Vidhul",
+//     age:19,
+//     place:"ollur"
+// }
+// console.log(students);
+// console.log(students.name);
+// var cars={
+//     Brand:"toyota",
+//     model:"2010 supra",
+//     price:10000000
+// }
+// var fruits =["apple","banana","orange",10]
+// console.log(fruits[0]);
+// console.log(fruits.length);
+// fruits.push("plum");
+// fruits.unshift("grapes")
+// fruits.pop();
+// fruits.shift();
+// console.log(frvar person =uits);
+// [
+//     {
+//         name:"Vidhul",
+//         age:18,
+//         place:"Thrissur"
+//     },
+//     {
+//         name:"Thejus",
+//         age:56,
+//         place:"chiyyaram"
+//     }
+// ]
+// console.log(person[1].name)
+// function add(a,b){
+//     var c=a+b;
+
+//     console.log(c)
+// }
+// add(1,2);
+// add(2,3);
+ 
+// const addition =(a,b)=>{
+//     var c=a+b;
+
+//     console.log(c)
+// addition(2,3)
+// }
+// const  mul=(a,b)=>{
+//     var c=a*b;
+
+//     console.log(c)
+// }
+// mul(1,2)
+// var x=20;
+// if(x>20){
+//     console.log("good")
+// }
+// else
+// console.log("bad");
+// for(var i=5;i++)
+// {1;i<=
+//     console.log(i);
+// }
+var fruits =["apple","banana","orange","karikku"];
+// for(let i=0;i<fruits.length;i++){
+//     console.log(fruits[i])
+// }
+
+// let p=0;
+// while(p<fruits.length){
+//     console.log(fruits[p]);
+//     p++;
+// }
+i=0;
+do{
+    console.log(fruits[i]);
+    i++
+}while(i<fruits.length)
